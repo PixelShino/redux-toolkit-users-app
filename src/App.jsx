f0c3d5cc8142430219e37e831761c1390dd5db4b
Goldobin <goldobin.dmitry@bk.ru>
2025-05-16 16:36:22 +0300

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,30 @@ function App() {
       <AddUserForm />
       <SelectedUserDetails />
       <UserList />
+      
+      <footer className="app-footer">
+        <a>
+          <p className='text-center text-gray-400'>&copy; Dmitrii Goldobin</p>
+        </a>
+        <a
+          href='https://github.com/PixelShino'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <p className='text-center text-gray-400'>
+            <i className='fab fa-github'></i> GitHub
+          </p>
+        </a>
+        <a
+          href='https://t.me/PixelShino'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <p className='text-center text-gray-400'>
+            <i className='fab fa-telegram'></i> Telegram
+          </p>
+        </a>
+      </footer>
     </div>
   );
 }
